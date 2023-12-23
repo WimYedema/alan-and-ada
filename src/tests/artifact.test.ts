eb0f3d736d71c1488dbc8c6afa2d78d2e9ed72d8
Wim <34412463+WimYedema@users.noreply.github.com>
2023-12-23 22:10:47 +0000

    First test passes
    use `npm run test` to run the test.

---
+++ src/tests/artifact.test.ts
@@ -0,0 +1,12 @@
+import {describe, expect, it, test} from '@jest/globals';
+import {Artifact} from '../core/artifact';
+import { tileSize } from '../core/resources';
+
+describe('Artifact', () => {
+  it('uses tile space for positioning in construction', () => {
+    let subject = new Artifact({x:1, y:4, name:"test"});
+    expect(subject.pos.x).toBe(1*tileSize);
+    expect(subject.pos.y).toBe(4*tileSize);
+    expect(subject.name).toBe("test");
+  });
+});
