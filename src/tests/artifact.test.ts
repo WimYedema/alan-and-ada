32a099e29b7c63a76b49f3c7d90242ba9034547b
Wim <34412463+WimYedema@users.noreply.github.com>
2024-01-14 14:49:58 +0000

    Test description change

--- src/tests/artifact.test.ts
+++ src/tests/artifact.test.ts
@@ -3,7 +3,7 @@ import { Artifact } from "../core/artifact";
 import { tileSize } from "../core/resources";
 
 describe("Artifact", () => {
-  it("uses tile space for positioning in construction", () => {
+  it("uses tile size for positioning in construction", () => {
     let subject = new Artifact({ x: 1, y: 4, name: "test" });
     expect(subject.pos.x).toBe(1 * tileSize);
     expect(subject.pos.y).toBe(-4 * tileSize);
