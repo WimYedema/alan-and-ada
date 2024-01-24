fceddf86f23c861de7b18ffd400fa8040ce36cb7
Wim <34412463+WimYedema@users.noreply.github.com>
2024-01-24 19:31:50 +0000

    Formatting

--- src/actors/potion.ts
+++ src/actors/potion.ts
@@ -68,10 +68,12 @@ export class Potion extends Artifact {
         this.potionColor == "purple" ? ex.Color.Magenta : ex.Color.Yellow,
       endColor: ex.Color.Transparent,
     });
-    if (this.killAfter(1.5, 10, () => {
-      this.scene.remove(emitter);
-    })) {
+    if (
+      this.killAfter(1.5, 10, () => {
+        this.scene.remove(emitter);
+      })
+    ) {
       this.scene.add(emitter);
-    };
+    }
   }
 }
